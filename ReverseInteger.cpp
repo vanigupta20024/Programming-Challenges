c8976bb573e7d31dae683ce2cc78dab8091255af
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-07-06 23:33:08 +0530

    Update ReverseInteger.cpp

--- ReverseInteger.cpp
+++ ReverseInteger.cpp
@@ -1,7 +1,8 @@
-/* Problem:
+/* Problem Statement:
 *  Given a 32-bit signed integer, reverse digits of an integer.
 *
-*  Note: Assume we are dealing with an environment which could only store integers within the 32-bit signed 
+*  Note: 
+*  Assume we are dealing with an environment which could only store integers within the 32-bit signed 
 *  integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 */
 
@@ -37,7 +38,8 @@ public:
         }
         
         // Given conditions:
-        //  integer range: [−2^31,  2^(31 − 1)]
+        // integer range: [−2^31,  2^(31 − 1)]
+        // if violated, return zero
         if(num > INT_MAX) return 0;
         if(num < INT_MIN) return 0;
         
