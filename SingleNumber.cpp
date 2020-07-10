c3fd2e08212ea61850fa30b82cc850f41b26646e
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-07-10 20:44:59 +0530

    Create SingleNumber.cpp

---
+++ SingleNumber.cpp
@@ -0,0 +1,27 @@
+// Given a non-empty array of integers, every element appears 
+// twice except for one. Find that single one.
+
+// Level - Easy
+// Easiest Approch:
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int x=0;
+        
+        /* 
+            XOR is basically subtraction:
+            7 ^ 7 = 0
+            7 ^ 5 = 2 and so on..
+            Also,
+            any number when XORed with 0, returns itself. So we populate the
+            integer x with first number and perform XOR will numbers.
+            The remaining number will be the one that does not cancel itself.
+        */
+        
+        for(int i=0;i<nums.size();i++){
+            x ^= nums[i];
+        }
+        return x;
+    }
+};
