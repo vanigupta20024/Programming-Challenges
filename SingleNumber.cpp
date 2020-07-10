30adea0dcd553a7c658c4717a85bc81fa80564cf
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-07-10 20:45:42 +0530

    Update SingleNumber.cpp

--- SingleNumber.cpp
+++ SingleNumber.cpp
@@ -1,5 +1,8 @@
 // Given a non-empty array of integers, every element appears 
 // twice except for one. Find that single one.
+// Example 1:
+//    Input: [4,1,2,1,2]
+//    Output: 4
 
 // Level - Easy
 // Easiest Approch:
