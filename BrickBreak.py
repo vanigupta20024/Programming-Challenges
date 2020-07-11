f0b8f5e8c3cbfc5c1cc1f427268f717d42ece8de
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-07-11 22:17:12 +0530

    Create BrickBreak.py

---
+++ BrickBreak.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING bricks
+#  2. INTEGER ball
+# This function will check the string at the 
+# index specified by the ball. If there is a
+# brick ("-") in that index, it will be replaced
+# with a space. Otherwise, nothing will be altered. 
+
+def brickBreak(bricks, ball):
+    string_ret = bricks[:ball]+" "+bricks[ball+1:]
+
+    if bricks[ball] == "-":
+        return(string_ret)
+    return bricks
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bricks = input()
+
+    ball = int(input().strip())
+
+    result = brickBreak(bricks, ball)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
