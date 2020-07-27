f8539d6cf61a3d7489cb8f1b51d2c5ced45c7dd5
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-07-27 10:51:38 +0530

    Update EulerTwelve.py

--- EulerTwelve.py
+++ EulerTwelve.py
@@ -1,23 +1,29 @@
-import time 
-
+import time
 start = time.time()
 
 n = 0
 d = 0
-while (True):
+
+def divisors(d):
+    
+    prev = 1
+    for i in range(2, d//2 + 1):
+        c = 0
+        if d%i == 0:
+            while d%i == 0:
+                c += 1
+                d /= i
+        if c != 0:
+            prev = prev * (c + 1)
+        if d == 1:
+            return prev
+    return prev
+
+while True:
     n += 1
     d += n
-
-    div = 0
-    for i in range(1, int(d ** 0.5) + 1):
-        if d % i == 0:
-            div += 2
-
-        if int(d ** 0.5) * int(d ** 0.5) == d:
-            div -= 1
-            
-    if div > 500:
+    
+    if divisors(d) > 500:
         print(d)
         break
-
 print(time.time() - start)
