6a7e680d188a7127f51e2b028d2f7735e0776f95
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-09-20 18:02:01 +0530

    Create bash_arithmetic.sh

---
+++ bash_arithmetic.sh
@@ -0,0 +1,10 @@
+# "read" reads user input
+
+read num1
+read num2
+
+# add double parenthesis for arithmetic evaluation
+echo $((num1 + num2))
+echo $((num1 - num2))
+echo $((num1 * num2))
+echo $((num1 / num2))
