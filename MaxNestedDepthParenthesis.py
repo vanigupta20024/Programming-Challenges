c0fac4598dddb4c924d5dbb57df96cb032ac3e13
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-10-29 20:11:26 +0530

    Create MaxNestedDepthParenthesis.py

---
+++ MaxNestedDepthParenthesis.py
@@ -0,0 +1,26 @@
+'''
+A string is a valid parentheses string (denoted VPS) if it meets one of the following:
+
+It is an empty string "", or a single character not equal to "(" or ")",
+It can be written as AB (A concatenated with B), where A and B are VPS's, or
+It can be written as (A), where A is a VPS.
+We can similarly define the nesting depth depth(S) of any VPS S as follows:
+
+depth("") = 0
+depth(C) = 0, where C is a string with a single character not equal to "(" or ")".
+depth(A + B) = max(depth(A), depth(B)), where A and B are VPS's.
+depth("(" + A + ")") = 1 + depth(A), where A is a VPS.
+For example, "", "()()", and "()(()())" are VPS's (with nesting depths 0, 1, and 2), and ")(" and "(()" are not VPS's.
+
+Given a VPS represented as string s, return the nesting depth of s.
+'''
+
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        op = mx = 0
+        for i in s:
+            if i is "(": op += 1
+            elif i is ")":
+                if mx < op: mx = op
+                op -= 1                
+        return mx
