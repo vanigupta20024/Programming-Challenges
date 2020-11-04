c5a322a9e5cb91d372d1e02de5b177da5ff7d50d
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-11-05 03:10:34 +0530

    Update SearchInBST.py

--- SearchInBST.py
+++ SearchInBST.py
@@ -6,11 +6,21 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        # iterative solution
+        while root is not None:
+            if val < root.val:
+                root = root.left
+            elif val > root.val:
+                root = root.right
+            elif val == root.val:
+                return root
+        return None
+# Recursive solution
 
-        if root is None or val == root.val:
-            return root
+#         if root is None or val == root.val:
+#             return root
         
-        if val < root.val:
-            return self.searchBST(root.left, val)
-        elif val > root.val:
-            return self.searchBST(root.right, val)
+#         if val < root.val:
+#             return self.searchBST(root.left, val)
+#         elif val > root.val:
+#             return self.searchBST(root.right, val)
