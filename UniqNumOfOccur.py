3e9507743c6dfcc0f37661888bb96b5dd05dfa8d
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-11-06 00:07:54 +0530

    Update UniqNumOfOccur.py

--- UniqNumOfOccur.py
+++ UniqNumOfOccur.py
@@ -1,5 +1,10 @@
 '''
+Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
+Example 1:
 
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
 '''
 
 class Solution:
