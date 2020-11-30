0bfe614a80d62771681f3b36eaf25cc14108aae4
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-11-30 16:32:51 +0530

    Create InsertSort.py

---
+++ InsertSort.py
@@ -0,0 +1,10 @@
+def insertion_sort(arr):
+    # first element is always sorted
+    for i in range(1, len(arr)):
+        temp = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > temp:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = temp
+    print(arr)
